5a8ac4de5d929039595d0d8c92bfc79cdfa1ea54
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 09:02:31 +0430

    CORS Library of Anar framework

---
+++ src/test/java/af/gov/anar/lib/cors/AnarLibCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
